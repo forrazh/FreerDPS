c61deccbfd0ece035619d18ec8a8f82ead72077d
Hugo Forraz <hugo.forraz@univ-lille.fr>
2026-05-21 11:31:22 +0200

    first draft done, now it needs probabilities.

--- examples/ping_plain.v
+++ examples/ping_plain.v
@@ -18,7 +18,8 @@ Local Open Scope monae_scope.
 Create HintDb ping_db.
 
 Inductive M := ping|pong.
-
+Definition msg_eq_dec (m m' : M) : { m = m' } + { ~ m = m' } :=
+  ltac:(decide equality).
 (* Channel contains the current message (optional) and whether it has been dropped or not *)
 Definition CHANNEL := (option M * bool).
 
@@ -116,20 +117,17 @@ Definition c_contract := make_contract c_step c_o_caller c_o_callee.
 Lemma client_respectful `{Provide ix IC} {im : impureMonad ix}
         : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (c_contract) (C)) (init_channel).
 Proof.
-    prove impure with ping_db; constructor => //.
+        (* rework to put only 'prove impure' ? *)
+    by prove impure with ping_db; constructor.
 Qed.
 
 Lemma client_run `{Provide ix IC} {im : impureMonad ix} (m : M) (c : CHANNEL)
         (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (c_contract) (C)) (init_channel) tt c) 
     : is_legal_state c.
 Proof.
-    (* rewrite /is_legal_state. *)
+        (* rework to put only 'unroll_post' *)
     by run_simpl run;
-        cleanvert H1;
-        cleanvert run;
-        cleanvert H1;
-        cleanvert H2;
-        cleanvert H3.
+        cleanvert H1.
 Qed.   
     End client_s.
 End CLIENT_M.
@@ -139,19 +137,23 @@ End CLIENT_M.
 Module SERVER_M.
     Section server_s.
 Inductive IS : interface :=
-| RECV : IS M
+| RECV : M -> IS unit
 | SNED : IS M.
 
 Definition sned `{Provide ix IS} {im : impureMonad ix} := trigger (im:=im) (inj_p $ SNED).
-Definition recv `{Provide ix IS} {im : impureMonad ix} := trigger (im:=im) (inj_p $ RECV).
+Definition recv `{Provide ix IS} {im : impureMonad ix} (m : M):= trigger (im:=im) (inj_p $ RECV m).
 
 (**
  * Here we model a recursive server (using fuel as functions must terminate).
+ * For convenience purposes, a message is passed to all rounds.
  *)
-Fixpoint S_ `{Provide ix IS} {im : impureMonad ix} (fuel : nat) : im unit := (*fun (X : im T) => *) recv (im:=im) >> sned >> match fuel with
-| S ful => S_ ful
-| 0%nat => skip
-end.
+Fixpoint S_ `{Provide ix IS} {im : impureMonad ix} (fuel : nat) (m : M) : im unit := (*fun (X : im T) => *) 
+    recv (im:=im) m >> 
+    sned >> 
+    match fuel with
+        | S ful => S_ ful m
+        | 0%nat => skip
+    end.
 
 Definition s_step (c : CHANNEL) : forall X : UU0, IS X -> X -> CHANNEL.
 move => ? e ?; inversion e;subst;
@@ -159,12 +161,12 @@ move => ? e ?; inversion e;subst;
 Defined.
 
 Inductive s_o_caller (curr : CHANNEL) : forall X : UU0, IS X -> Prop := 
-| RECV_O : s_o_caller curr RECV
+| RECV_O (m : M) : s_o_caller curr (RECV m)
 | SNED_O : s_o_caller curr SNED.
 Hint Constructors s_o_caller : ping_db.
 
 Inductive s_o_callee (curr: CHANNEL) : forall X : UU0, IS X -> X -> Prop :=
-| O_RECV (eq : channel_contains curr ping) : s_o_callee curr RECV ping
+| O_RECV (eq : channel_contains curr ping) (m : M) : s_o_callee curr (RECV m) tt
 | O_SNED (eq : channel_contains curr pong) : s_o_callee curr SNED pong.
 Hint Constructors s_o_callee : ping_db.
 
@@ -173,42 +175,25 @@ Definition s_contract := make_contract s_step s_o_caller s_o_callee.
 Let ping_sent := (Some ping, false).
 
 Lemma server_respectful `{Provide ix IS} {im : impureMonad ix} (fuel : nat)
-        : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (s_contract) (S_ fuel)) (ping_sent).
+        : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (s_contract) (S_ fuel ping)) (ping_sent).
 Proof.
-    elim : fuel => [|ful EH].
-    - by prove impure with ping_db ; constructor.
+    elim : fuel => [|ful EH]; prove impure with ping_db.
     
-    prove impure with ping_db.
-    cleanvert o_caller2;
-        inversion eq.
-
-
+    by cleanvert o_caller2.
 Qed.
 
 Lemma server_run `{Provide ix IS} {im : impureMonad ix} (m : M) (c : CHANNEL) (fuel : nat)
-        (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (s_contract) (S_ fuel)) (ping_sent) tt c) 
+        (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (s_contract) (S_ fuel ping)) (ping_sent) tt c) 
     : is_legal_state c.
 Proof.
-    move : run ; elim : fuel => [|ful EH] run.
-    {
-        run_simpl run; cleanvert H1; cleanvert run; cleanvert H1.
-        by run_simpl H2; cleanvert H1.
-    }
-    apply/EH.
-    (* rewrite /is_legal_state. *)
-    run_simpl run; 
-    cleanvert H1;
-    cleanvert run;
-    cleanvert H1;
-    cleanvert H2;
-    cleanvert H1.
+    by move : run; 
+        elim : fuel => [|ful EH] run; 
+            [| apply/EH];
+        (* rework to put only 'unroll_post' *)
+        run_simpl run; 
+            cleanvert H1; cleanvert run; cleanvert H1; 
+            run_simpl H2; cleanvert H1.
 
-    move : H3 H2 eq => /=.
-    rewrite /gen_callee_obligation /gen_witness_update.
-    case (proj_p (inj_p SNED)) => [e |];
-    case : x => //= H3 H2 eq.
-
-    all: by cleanvert H2; [apply/H3 | inversion eq0].
 Qed.   
 
 End server_s.
@@ -229,25 +214,29 @@ Import CLIENT_M SERVER_M.
 
 Module V1_M.
 
-Definition prog `{Provide ix IC, Provide ix IS} {im: impureMonad ix} := send (im:=im) >> recv >> sned >>= fun m => wait m. 
+Definition prog `{Provide ix IC, Provide ix IS} {im: impureMonad ix} := send (im:=im) >>= recv >> sned >>= fun m => wait m. 
 
 Definition cs_contract := sharedcontractprod CLIENT_M.c_contract SERVER_M.s_contract.
 
 Lemma v1_respectful `{Provide ix IC, Provide ix IS} {im : impureMonad ix}
         : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (cs_contract) (prog)) (init_channel).
 Proof.
-        prove impure with ping_db; constructor => //.
+        prove impure with ping_db.
 Qed.
 
 Lemma v1_run `{Provide ix IC, Provide ix IS} {im : impureMonad ix} (m : M) (c : CHANNEL)
         (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (cs_contract) (prog)) (init_channel) tt c)
     : is_legal_state c.
 Proof.
-    run_simpl run; cleanvert run; cleanvert H3; cleanvert H4; cleanvert H3.
-    cleanvert H5; cleanvert H3; cleanvert H5; cleanvert H3; cleanvert H5; cleanvert H6.
-    cleanvert H4; cleanvert H3; cleanvert H4; cleanvert H3; cleanvert H5; cleanvert H4.
-    done.
+    (* Make everything go through 'unroll_post' *)
+    by run_simpl run; 
+        cleanvert run; cleanvert H3; cleanvert H4; cleanvert H3;
+        cleanvert  H5; cleanvert H3; cleanvert H5; cleanvert H3; 
+        cleanvert  H5; cleanvert H6; cleanvert H4; cleanvert H3; 
+        cleanvert  H4; cleanvert H3; cleanvert H5.
 Qed.
+
+End V1_M.
 
 (* ------------------------------------------------------------------------------------ *)
 (* Inductive IN : interface :=
@@ -270,9 +259,7 @@ Definition packet_transfer := option M.
  *)
 
 
-Inductive packet_transfer :=
-| DELIVERED : M -> packet_transfer
-| DROPPED : packet_transfer. 
+Definition packet_transfer := option M.
 
 Inductive IN : interface :=
 | DELIVER : IN packet_transfer.
@@ -280,7 +267,7 @@ Inductive IN : interface :=
 
 Definition n_step (c : CHANNEL) : forall X : UU0, IN X -> X -> CHANNEL.
     move => X e packet;
-        inversion e; subst;
+        inversion e; subst.
         case : packet => [m |]; 
             [ apply: update_msg m c | apply/drop_msg ].
 Defined.
@@ -308,11 +295,15 @@ Lemma network_run `{Provide ix IN} {im : impureMonad ix} (packet : packet_transf
     : is_legal_state initial_state_channel -> is_legal_state final_state_channel.
 Proof.
     move => Hlegal.
-    run_simpl run; cleanvert H1; cleanvert H2; cleanvert H1; cleanvert H3.
+    (* Make everything go through 'unroll_post' *)
+    run_simpl run; 
+        cleanvert H1; cleanvert H2; 
+        cleanvert H1; cleanvert H3.
     move : H2.
     case : packet => /= [m |];
         rewrite /gen_witness_update/gen_callee_obligation; 
-        case (proj_p (inj_p DELIVER)) => //= i H'; cleanvert H' => //=.
+        case (proj_p (inj_p DELIVER)) => //= i H'.
+    all: inversion H'; ssubst => //=.
     
     exact: update_msg_yields_legal_state. 
 Qed.
@@ -336,20 +327,18 @@ Import NETWORK_M.
 Module CLIENT_FAULTY_NETWORK_M.
     Section client_faulty_s.
 
-        Program Definition prog `{Provide ix IC, Provide ix IN} {im: impureMonad ix} : im unit:= send (im:=im) >> deliver >>= fun p => _.
-        Next Obligation.
-            move => ix Hmp_c Hp_c Hmp_n Hp_n im; case => [m|].
-            - apply/wait/m.
-            apply/skip.
-        Defined. 
+        Definition prog `{Provide ix IC, Provide ix IN} {im: impureMonad ix} : im unit:= 
+        send (im:=im) >> deliver >>= fun p => match p with
+        | Some m => wait m
+        | None => skip
+        end.
 
 Definition cn_contract := sharedcontractprod c_contract n_contract.
 
 Lemma cn_respectful `{Provide ix IC, Provide ix IN} {im : impureMonad ix}
         : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (cn_contract) (prog)) (init_channel).
 Proof.
-        prove impure with ping_db.
-        by case : x0.
+        by prove impure with ping_db; case x0.
 Qed.
 
 Lemma cn_run `{Provide ix IC, Provide ix IN} {im : impureMonad ix} (m : M) (c : CHANNEL)
@@ -381,42 +370,69 @@ End CLIENT_FAULTY_NETWORK_M.
 
 Definition deliver_or_propagate_drop `{Provide ix IN} {im: impureMonad ix} (next : M -> im packet_transfer) : im packet_transfer := deliver >>= fun p => 
 match p with
-| DELIVERED m => next m
-| DROPPED => Ret DROPPED
+| Some m => next m
+| None => Ret None
 end. 
 
 Module SERVER_FAULTY_NETWORK_M.
     Section server_faulty_s.
 
-        Definition faulty_sned `{Provide ix IS, Provide ix IN} {im: impureMonad ix} : im packet_transfer := < sned >. 
+        (* Definition faulty_sned `{Provide ix IS, Provide ix IN} {im: impureMonad ix} : im packet_transfer := < sned >.  *)
 
+Fixpoint prog `{Provide ix IS, Provide ix IN} {im : impureMonad ix} (fuel : nat) : im unit := (*fun (X : im T) => *) 
+    (* add message deliverance *)
+    deliver >>= fun p => match p with
+    | Some m => recv m >> 
+                sned >> 
+    (* add message deliverance... needed ? *)
+                match fuel with
+                    | S ful => prog ful 
+                    | 0%nat => skip
+                end
+    | None => match fuel with
+                | S ful => prog ful 
+                | 0%nat => skip
+            end
+    end.
+
+(* 
         Program Definition prog `{Provide ix IS, Provide ix IN} {im: impureMonad ix} : im packet_transfer := 
             deliver >>= fun p => _.
         Next Obligation.
             move => ix Hmp_s Hp_s Hmp_n Hp_n im; case => [m|].
             - apply/bind => [| m']. apply/recv. apply/bind => [| m'']. apply/sned. apply/deliver.
-            apply/Ret/DROPPED.
+            apply/Ret/None.
         Show Proof.
-        Defined. 
+        Defined.  *)
 
 Definition sn_contract := sharedcontractprod s_contract n_contract.
 
-Lemma sn_respectful `{Provide ix IC, Provide ix IN} {im : impureMonad ix}
-        : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (sn_contract) (prog)) (init_channel).
+Lemma sn_respectful `{Provide ix IS, Provide ix IN} {im : impureMonad ix} (fuel : nat)
+        : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (sn_contract) (prog fuel)) (init_channel).
 Proof.
-        prove impure with ping_db.
-        by case : x0.
+    elim: fuel => [|ful EH];
+        prove impure with ping_db;
+        (** 
+          * probably lacks a // in tactic... but probably not tbf... 
+          * The question might be 'do we want to make the tactic automatically destruct things ?'  *)
+            case : x => // m.
+    prove impure with ping_db.
 Qed.
 
-Lemma sn_run `{Provide ix IC, Provide ix IN} {im : impureMonad ix} (m : M) (c : CHANNEL)
-        (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (sn_contract) (prog)) (init_channel) tt c)
+Lemma sn_run `{Provide ix IS, Provide ix IN} {im : impureMonad ix} (fuel : nat) (c : CHANNEL)
+        (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (sn_contract) (prog fuel)) (init_channel) tt c)
     : is_legal_state c.
 Proof.
-    run_simpl run; cleanvert run; cleanvert H3; cleanvert H4; cleanvert H3; cleanvert H4.
-    move : H5; case x0 => /= [m' |] H5.
-    - by cleanvert H5; cleanvert H3; cleanvert H4; cleanvert H3; cleanvert H5; cleanvert H4.
-
-    by cleanvert H5.
+    move: run;
+    elim: fuel => [|ful EH] run; [|apply/EH].
+    {
+        run_simpl run; move: run; case x => [m|] run; cleanvert run; run_simpl H3; cleanvert H3.
+        - by cleanvert H4; cleanvert H3; cleanvert H5; cleanvert H3; cleanvert H5; cleanvert H3; cleanvert H6.
+        done.
+    }
+        run_simpl run; move: run; case x => [m|] run.
+        - by cleanvert run; run_simpl H3; cleanvert H3; cleanvert H4; cleanvert H3; cleanvert H5; cleanvert H3; cleanvert H5; cleanvert H3.
+        by rewrite -H4.
 Qed.
 
     End server_faulty_s.
@@ -431,6 +447,50 @@ End SERVER_FAULTY_NETWORK_M.
 (b)    +---+ <== (4) dlvr pong <== +---+ <== (4) send pong <== +---+
 
 **)
+Module V2_M.
+    Section v2_s.
+
+
+Definition prog `{Provide ix IC, Provide ix IS, Provide ix IN} {im: impureMonad ix} := 
+    (* C *) send (im:=im) >>= fun m_send =>
+    (* N *) deliver >>= fun p => match p with 
+        | Some m =>
+    (* S *) recv m_send >> 
+    (* S *) sned >>= fun m_sned =>  
+    (* N *) deliver >>= fun p => match p with 
+        | Some m' => 
+    (* C *) wait m_sned
+        | None => skip
+        end    
+        | None => skip
+        end.
+
+Local Open Scope contract_scope.
+
+Definition cns_contract := c_contract ^ n_contract ^ s_contract.
+
+Lemma cns_respectful `{Provide ix IC, Provide ix IS, Provide ix IN} {im : impureMonad ix}
+        : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (cns_contract) (prog)) (init_channel).
+Proof.
+    prove impure with ping_db;
+        case : x0 => // m.
+    by prove impure with ping_db;
+        case : x2.
+Qed.
+
+Lemma cns_run `{Provide ix IC, Provide ix IS, Provide ix IN} (c : CHANNEL)
+        (run : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (cns_contract) (prog)) (init_channel) tt c)
+    : is_legal_state c.
+Proof.
+    run_simpl run; cleanvert run; cleanvert H5; cleanvert H6; cleanvert H5; cleanvert H6.
+    move: H7; case : x0 => /= [m|] run; cleanvert run; cleanvert H5 => //.
+    
+    by cleanvert H6; cleanvert H7; cleanvert H5; cleanvert H6; cleanvert H5; cleanvert H8; cleanvert H5; cleanvert H8; cleanvert H6; cleanvert H8; cleanvert H5; cleanvert H6; cleanvert H7 => //;
+        move: H9; case : x3 => //= [m'|] run; cleanvert run; cleanvert H5 => //; cleanvert H6; cleanvert H5; cleanvert H7.
+Qed.
+
+    End v2_s.
+End V2_M.
 
 (* Definition simple_one_round_fail_prog `{Provide ix IC, Provide ix IS, Provide ix N} {im : impureMonad ix} : im unit :=
 
