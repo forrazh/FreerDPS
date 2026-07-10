3e47c9476e418b5a73545b4c865f7f018bb893dc
Hugo Forraz <hugo.forraz@univ-lille.fr>
2026-07-10 11:42:11 +0200

    Refactor hoarefacts proofs.

--- theories/HoareFacts.v
+++ theories/HoareFacts.v
@@ -55,6 +55,10 @@ Lemma to_hoare_request_postI `{MayProvide ix i} {im : impureMonad ix}
        (gen_witness_update c ω e x).
 Proof. by rewrite to_hoare_requestE; exact: interface_to_hoare_postI. Qed.
 
+Lemma pair_eqE {A B} (x x' : A) (y y' : B) :
+  (x, y) = (x', y') -> x = x' /\ y = y'.
+Proof. by move=> [] /=; split. Qed.
+
 (** * General Lemmas *)
 
 Lemma to_hoare_step `{MayProvide ix i} `(c : contract i Ω)
@@ -64,11 +68,7 @@ Lemma to_hoare_step `{MayProvide ix i} `(c : contract i Ω)
   : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) c (f x)) (gen_witness_update c ω e x).
 
 Proof.
-  destruct hpre as [hbefore hafter].
-  apply hafter.
-  cbn.
-  unfold gen_callee_obligation, gen_witness_update in *.
-  now destruct proj_p.
+  by case: hpre=> [_ next]; apply/next/interface_to_hoare_postI.
 Qed.
 
 #[global] Hint Resolve to_hoare_step : freespec.
@@ -81,32 +81,14 @@ Lemma to_hoare_pre_bind_assoc `{MayProvide ix i} `(c : contract i Ω)
   : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) c (impure_bind p f)) ω.
 
 Proof.
-  revert ω Hp run.
-  induction p; intros ω Hp run.
-  + now apply run.
-  + cbn in Hp.
-    destruct Hp as [He Hn].
-    change (impure_bind (request_then e f0) f)
-      with (impure_bind (request_then e (fun x => f0 x)) f).
-    split.
-    ++ exact He.
-    ++ intros x ω' Hpost.
-       specialize Hn with x ω'.
-       destruct Hpost.
-       rewrite -> H2 in *.
-       assert (Hpre : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) c (f0 x)) (gen_witness_update c ω e x))
-         by now apply Hn.
-       apply H0; [ apply Hpre |].
-       intros y ω'' Hpost.
-       apply run.
-       cbn.
-       exists x.
-       exists ω'.
-       split; [split |].
-       +++ exact H1.
-       +++ exact H2.
-       +++ rewrite H2.
-           exact Hpost.
+  move: p ω Hp run.
+  elim=>/=[in_a | Y e k IH] ω Hp run.
+  + by apply: run; split.
+  + move: Hp=> [caller next]; split=> // x ω' [callee witness]; apply: IH => [| y ω''].
+    ++ by apply: next; split.
+    ++ move: witness run => -> + Hnext; apply.
+       exists x, (gen_witness_update c ω e x).
+       by split; last exact: Hnext; split.
 Qed.
 
 Lemma to_hoare_post_bind_assoc `{MayProvide ix i} `(c : contract i Ω)
@@ -116,15 +98,10 @@ Lemma to_hoare_post_bind_assoc `{MayProvide ix i} `(c : contract i Ω)
     post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) c p) ω y ω'' /\ post (to_hoare c (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) $ f y) ω'' x ω'.
 
 Proof.
-move: ω Hp; elim p=>[in_a|Y j k IH] ω.
-- by exists in_a, ω.
-case=>y [ω'' [Hp1 ]].
-move:IH=>/[apply].
-move=> [z [ω''' [Hp2 Hp3]]].
-exists z, ω'''.
-split=>//.
-exists y, ω''.
-by split.
+  move: p ω Hp; elim=> [in_a | Y e k IH] ω.
+  - by exists in_a, ω.
+  by move=> [y [ω'' [ostep +]]]=> /IH [z [ω''' [Hprefix Hsuffix]]];
+    exists z, ω'''; split=> //; exists y, ω''; split.
 Qed.
 
 Lemma to_hoare_contractprod `{Provide ix i, Provide ix j}
@@ -134,21 +111,14 @@ Lemma to_hoare_contractprod `{Provide ix i, Provide ix j}
   : pre (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure ix) (ci * cj) p) (ωi, ωj).
 
 Proof.
-  revert ωi prei ωj prej.
-  induction p; intros ωi prei ωj prej.
-  + auto.
-  + destruct prei as [calleri Hcalleei].
-    destruct prej as [callerj Hcalleej].
-    split.
-    ++ now split.
-    ++ intros x [ωi' ωj'] [[calleei calleej] equωs].
-       cbn in equωs.
-       inversion equωs; subst.
-       apply H3.
-       +++ apply Hcalleei.
-           now split.
-       +++ apply Hcalleej.
-           now split.
+  move: p ωi prei ωj prej; elim=> //= Y e k IH ωi [calleri nexti] ωj [callerj nextj]; split => [|].
+  + exact: (conj calleri callerj).
+  move=> y [ωi' ωj']; rewrite -interface_to_hoare_postE=> [[[calleei calleej] witnesses]].
+  have [wi wj] := pair_eqE _ _ _ _ witnesses.
+  
+  by move: IH witnesses wi wj=> + -> ??; 
+    apply; [apply: nexti | apply: nextj]; 
+    rewrite -interface_to_hoare_postE; split.
 Qed.
 
 #[global] Hint Resolve to_hoare_contractprod : freespec.
@@ -163,7 +133,8 @@ Proof.
   move: ω1.
   elim: p=> [a | B e k IH] ω1. 
   - by split.
-  rewrite /=/gen_caller_obligation/gen_callee_obligation/gen_witness_update/=.
+  rewrite /= /gen_caller_obligation /gen_callee_obligation
+              /gen_witness_update /=.
   rewrite (caller_equ ω1 B e).
   setoid_rewrite (callee_equ ω1 B e).
   split => [[ocaller onext] | [ocaller onext]];
@@ -177,29 +148,19 @@ Qed.
 
 Lemma contract_equ_post `(c1 : contract i Ω1) `(c2 : contract i Ω2)
    `(equ : contract_equ c1 c2) (ω1 ω1' : Ω1)
-   `(p : impure i a) (x : a)
+   `(p : impure i A) (x : A)
     (post1 : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure i) c1 p) ω1 x ω1')
   : post (to_hoare (im:=ImpureModule_acto__canonical__Impure_MonadImpure i) c2 p) (contract_iso_lr equ ω1) x (contract_iso_lr equ  ω1').
 
 Proof.
-  induction equ.
-  cbn in *.
-  revert x ω1 ω1' post1.
-  induction p; intros y ω1 ω1' post1.
-  + destruct post1 as [xequ ω1equ].
-    cbn.
-    now subst.
-  + cbn in post1.
-    destruct post1 as [x [ω1'' [[ocallee owitness] post1]]].
-    eapply H in post1.
-    exists x.
-    exists (f ω1'').
-    split; auto.
-    cbn.
-    repeat split.
-    ++ eapply callee_equ; eauto.
-    ++ rewrite owitness.
-       apply witness_equ.
+  elim:equ=> [f g iso1 iso2 caller_equ callee_equ witness_equ].
+  move: p x ω1 ω1' post1.
+  elim=>[in_a | B e k IH]=> a ω1 ω1' [b witness] /=.
+  + by subst a; subst ω1'; split.
+  + move: witness=> [ω1'' [[ocallee owitness] +]] /= => /IH post1.
+    exists b, (f ω1''); split; [split|exact: post1].
+    ++ exact/(callee_equ ω1 B e).
+    ++ by rewrite owitness witness_equ.
 Qed.
 
 #[global] Hint Resolve contract_equ_post : freespec.
