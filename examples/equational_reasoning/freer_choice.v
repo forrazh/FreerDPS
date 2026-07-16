e85e9780b5e5c94c7f045aca21c21b6e49a14a5b
Hugo Forraz <hugo.forraz@univ-lille.fr>
2026-07-16 09:21:49 +0200

    add a first freer prob ping

--- examples/equational_reasoning/freer_choice.v
+++ examples/equational_reasoning/freer_choice.v
@@ -32,7 +32,7 @@ Inductive FlipEff : UU0 -> UU0 :=
 Variable M : (freerMonad FlipEff).
 
 
-Definition flipf  `{Provide ix FlipEff} {im : freerMonad ix} (p:{prob R}) : im bool := (trigger $ flip_e p).
+Definition flipf `{Provide ix FlipEff} {im : freerMonad ix} (p : {prob R}) : im bool := trigger $ flip_e p.
 
 Variable fl : probMonad R.
 
@@ -53,7 +53,7 @@ Proof.
   by rewrite /flipf denote_request /denote_flip_effect.
 Qed.
 
-Definition choicef {X} (p : {prob R}) (a b : M X) := flipf p >>= (fun b0 => if b0 then a else b).
+Definition choicef (p : {prob R}) {X} (a b : M X) := flipf p >>= (fun b0 => if b0 then a else b).
 
 Lemma denote_choicefE (X : UU0) (p : {prob R}) (a b : M X) :
     denote (s := M) fl denote_flip_effect X (choicef p a b) =
@@ -107,6 +107,20 @@ Proof.
   by [].
 Qed.
 
+Lemma denote_choice_bindDlE (A B : UU0) (p : {prob R}) (a b : M A)
+    (f : A -> M B) :
+    denote fl denote_flip_effect B ((choicef p a b) >>= f) =
+      denote fl denote_flip_effect B (choicef p (a >>= f) (b >>= f)).
+Proof.
+  rewrite denote_bind denote_choicefE.
+  rewrite [in RHS]denote_choicefE.
+  under [in RHS]eq_bind do rewrite !denote_bind /ssrfun.comp.
+  rewrite /ssrfun.comp bindA.
+  congr bind.
+  apply/boolp.funext=> b0.
+  by case: b0.
+Qed.
+
 
 (* 5th step : Choice equiv laws *)
 Inductive choice_rel :forall `[X : UU0] (m1 m2 : M X), Prop :=
@@ -128,7 +142,47 @@ Inductive choice_rel :forall `[X : UU0] (m1 m2 : M X), Prop :=
 | equiv_refl : forall (A:UU0) (m : M A), choice_rel m m
 | equiv_sym : forall (A:UU0) (m n : M A), choice_rel m n -> choice_rel n m
 | equiv_trans : forall (A:UU0) (m n o : M A), choice_rel m n -> choice_rel n o -> choice_rel m o
+| rchoice_bindDl : forall (A B : UU0) (p : {prob R}) (a b : M A)
+    (f : A -> M B),
+    choice_rel ((choicef p a b) >>= f) (choicef p (a >>= f) (b >>= f))
 .
+
+Lemma rchoice_bindDl_trans (A B : UU0) (p : {prob R}) (a b : M A)
+    (f : A -> M B) (c : M B) :
+    choice_rel (choicef p (a >>= f) (b >>= f)) c ->
+    choice_rel ((choicef p a b) >>= f) c.
+Proof.
+  move=> choice_c.
+  apply: equiv_trans choice_c.
+  exact: rchoice_bindDl.
+Qed.
+
+Lemma rchoice_bindDl_choice_congr (A B : UU0) (p : {prob R}) (a b : M A)
+    (f : A -> M B) (a' b' : M B) :
+    choice_rel (a >>= f) a' ->
+    choice_rel (b >>= f) b' ->
+    choice_rel ((choicef p a b) >>= f) (choicef p a' b').
+Proof.
+  move=> af bg.
+  apply: rchoice_bindDl_trans.
+  rewrite /choicef.
+  apply: equiv_bind_congr.
+  - exact: equiv_refl.
+  by case; [exact: af | exact: bg].
+Qed.
+
+Lemma equiv_bind_congr_trans (A B : UU0) (a b : M A) (f g : A -> M B)
+    (c : M B) :
+    choice_rel a b ->
+    (forall x, choice_rel (f x) (g x)) ->
+    choice_rel (b >>= g) c ->
+    choice_rel (a >>= f) c.
+Proof.
+  move=> ab fg choice_c.
+  apply: equiv_trans choice_c.
+  exact: equiv_bind_congr.
+Qed.
+
 (* 6th step : Equiv correct *)
 Lemma equiv_correct : forall (X : UU0) (m1 m2 : M X),
      @choice_rel X m1 m2 ->
@@ -136,7 +190,12 @@ Lemma equiv_correct : forall (X : UU0) (m1 m2 : M X),
 Proof.
   move=>X m1 m2 H.
   (* rewrite/denote/=.  *)
-  elim: H=> [*|*|*|*|*|*|????->|?????->?->] //=; last first; rewrite ?denote_bind.
+  elim: H=> [*|*|*|*|*|*|????->|?????->?->|*] //=;
+    last first; rewrite ?denote_bind.
+  - rewrite bindA.
+    congr bind.
+    apply/boolp.funext=> b0.
+    by case: b0; rewrite /ssrfun.comp denote_bind.
   - by congr bind; [| apply/boolp.funext].
 
   rewrite denote_choiceA_leftE denote_choiceA_rightE.
